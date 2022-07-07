ce53e74fabb12264a3b61feb78afe93c315ad340
Tim Sneath <timsneath@google.com>
2022-07-07 15:11:43 -0700

    Add module

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/timsneath/hellogo/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello again")
+	// fmt.Println("Hello again")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello world", "Hello World"))
 }
